9fbb8409f46b073a1e5addacc5383419727fd161
alexFiorenza <47326217+alexFiorenza@users.noreply.github.com>
2019-10-29 23:25:07 -0300

    Add Player script
    Added rigidbody colliders and first script for movement

---
+++ SpaceMan Platzi/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float jumpforce = 6f;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)||Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+    }
+
+    void Jump()
+    {
+        rigidBody.AddForce(Vector2.up*jumpforce,ForceMode2D.Impulse);
+    }
+}
