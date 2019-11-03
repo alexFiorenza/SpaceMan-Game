6f1e08944e216a2ff3d87aceb9d8fccb2b5eac7c
alexFiorenza <47326217+alexFiorenza@users.noreply.github.com>
2019-11-03 09:26:08 -0300

    Physics and states

---
+++ SpaceMan Platzi/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Este es un numerador que sirve para ponerlo en diferentes estados
+public enum GameState
+{
+    menu,
+    inGame,
+    gameOver
+}
+public class GameManager : MonoBehaviour
+{
+    public GameState currentGameState = GameState.menu;
+    public static GameManager sharedInstance; //Un singletone
+    PlayerController controller;
+    private void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) && currentGameState!=GameState.inGame)
+        {
+            StartGame();
+        }
+    }
+
+
+    //Empezar el juego en el menu
+    public void StartGame()
+    {
+        SetGameState(GameState.inGame);
+    }
+
+    //Volver al menu
+    public void BackToMenu()
+    {
+        SetGameState(GameState.menu);
+    }
+
+    //Perder
+    public void GameOver()
+    {
+        SetGameState(GameState.gameOver);
+    }
+
+    private void SetGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+            
+        }else if (newGameState == GameState.inGame)
+        {
+            controller.StartGame();
+        }
+        else
+        {
+            
+        }
+
+        this.currentGameState = newGameState;
+    }
+}
--- SpaceMan Platzi/Assets/Scripts/PlayerController.cs
+++ SpaceMan Platzi/Assets/Scripts/PlayerController.cs
@@ -6,27 +6,100 @@ public class PlayerController : MonoBehaviour
 {
     public float jumpforce = 6f;
     private Rigidbody2D rigidBody;
+    public LayerMask groundMask; //Creamos una variable LayerMask que referiense a la capa del suelo
+    private Animator anim;
+    private const string STATE_ALIVE = "isAlive";
+    private const string STATE_ON_THE_GROUND = "isOnTheGround";
+    public float runningSpeed = 2.0f;
+    private SpriteRenderer spriteRender;
+    Vector3 startPosition;
+
 
     private void Awake()
     {
         rigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRender = GetComponent<SpriteRenderer>();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPosition = this.transform.position;
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)||Input.GetMouseButtonDown(0))
+        Debug.DrawRay(this.transform.position, Vector2.down * 1.5f, Color.green);
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
         {
-            Jump();
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                Jump();
+            }
         }
+        
+        anim.SetBool(STATE_ON_THE_GROUND, IsTouchingTheGround());
     }
 
     void Jump()
     {
-        rigidBody.AddForce(Vector2.up*jumpforce,ForceMode2D.Impulse);
+        if (IsTouchingTheGround())
+        {
+            rigidBody.AddForce(Vector2.up * jumpforce, ForceMode2D.Impulse);    
+        }
+            
+    }
+    private void FixedUpdate()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if (rigidBody.velocity.x < runningSpeed)
+            {
+                if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+                {
+                    rigidBody.velocity = new Vector2(runningSpeed, rigidBody.velocity.y);
+                    spriteRender.flipX = false;
+                }
+                if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+                {
+                    rigidBody.velocity = new Vector2(-runningSpeed, rigidBody.velocity.y);
+                    spriteRender.flipX = true;
+                }
+            }
+            
+        }
+        
+
+    }
+
+
+    //Nos devuelve un booleano para saber  si el personaje esta tocando o no el suelo
+    bool IsTouchingTheGround()
+    {
+        if (Physics2D.Raycast(this.transform.position, Vector2.down,1.5f, groundMask)) //origen direccion distancia maxima y layerMask
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    public void Die()
+    {
+        this.anim.SetBool(STATE_ALIVE, false);
+    }
+
+    public void StartGame()
+    {
+        anim.SetBool(STATE_ALIVE, true);
+        anim.SetBool(STATE_ON_THE_GROUND, true);
+        Invoke("RestartPosition", 0.4f);   
+    }
+
+    void RestartPosition()
+    {
+        this.transform.position = startPosition;
+        rigidBody.velocity = Vector2.zero;
     }
 }
