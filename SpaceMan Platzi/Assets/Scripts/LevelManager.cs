c8614dc7595519f5671039bf9f915c256de6a716
alexFiorenza <47326217+alexFiorenza@users.noreply.github.com>
2019-11-04 19:01:32 -0300

    LevelBlocks

---
+++ SpaceMan Platzi/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager sharedInstance;
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
+    public List<LevelBlock> currentLevelBlock = new List<LevelBlock>();
+
+    public Transform levelStartPosition;
+
+    private void Awake()
+    {
+        if (sharedInstance == null)
+        {
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateInitialBlocks();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddLevelBlock()
+    {
+        int randomIdx = Random.Range(0, allTheLevelBlocks.Count);
+
+        LevelBlock block;
+
+        Vector3 spawnPosition = Vector3.zero;
+
+        if (currentLevelBlock.Count == 0) //todaviamos no añadimos ningun bloque
+        {
+            block = Instantiate(allTheLevelBlocks[0]);
+            spawnPosition = levelStartPosition.position;
+        }
+        else
+        {
+            block = Instantiate(allTheLevelBlocks[randomIdx]);
+            spawnPosition = currentLevelBlock[currentLevelBlock.Count-1].endPoint.position;
+        }
+
+        block.transform.SetParent(this.transform,false); //Todos los bloques quedaran como hijos del LevelManager
+        Vector3 correction = new Vector3(spawnPosition.x-block.startPoint.position.x,spawnPosition.y-block.startPoint.position.y,0);
+        block.transform.position = correction;
+        currentLevelBlock.Add(block);
+    }
+
+    public void RemoveLevelBlock()
+    {
+
+    }
+    public void RemoveAllLevelBlocks()
+    {
+
+    }
+    public void GenerateInitialBlocks()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+            AddLevelBlock();
+        }
+    }
+}
